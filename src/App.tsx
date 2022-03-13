9da8052be6b036e91acb190c1b9c4bc95e12c17f
nanaeomata <nanae.omata@breakedge.jp>
2022-03-13 23:38:13 +0900

    state and props 187

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
+
+// function App() {
+//   return (
+//     <div className="App">
+//
+//     </div>
+//   );
+// }
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text: text}]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
